00de4d0790d27aa20ed82f962930980ba739bd36
Mohammad Tarique Aalam <riz939@gmail.com>
2020-12-28 15:02:24 +0530

    changes for without view chikdren

--- src/app/angular-example/view-children-example/view-children-example.component.ts
+++ src/app/angular-example/view-children-example/view-children-example.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-children-example.component.scss']
 })
 export class ViewChildrenExampleComponent implements OnInit {
-
+display:boolean=true;
   constructor() { }
+  data:any;
+  displayStatus:boolean=true;
 
   ngOnInit(): void {
+    this.data=[{rollNo:1,name:"abcd",subject:["math","English","Urdu"]}
+    ,{rollNo:2,name:"xyz",subject:["math","English","Urdu"]}
+    ,{rollNo:3,name:"pqr",subject:["math","English","Urdu"]}
+    ,{rollNo:4,name:"lmn",subject:["math","English","Urdu"]}
+];
+  }
+
+  viewAllSubjects(status:boolean){
+    if(status){
+      this.data.forEach((element: any) => element['view']=true);
+      this.displayStatus=false;
+    }else{
+      this.data.forEach((element: any) => element['view']=false);
+      this.displayStatus=true;
+    }
+    
   }
 
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [  
-{path : 'home',loadChildren: () => import('./home-dashboard/home-dashboard.module')
-.then(mod => mod.HomeDashboardModule)},
-{path : '', redirectTo : '/home', pathMatch : 'full'}];
+  {
+    path: 'home', loadChildren: () => import('./home-dashboard/home-dashboard.module')
+      .then(mod => mod.HomeDashboardModule)
+  },
+  {
+    path: 'angular-example', loadChildren: () => import('./angular-example/angular-example.module')
+      .then(mod => mod.AngularExampleModule)
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
